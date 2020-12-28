a456342af51874fae7e828b3fcc8da31019187e5
Murilo Araujo <muernandes@icloud.com>
2020-12-28 17:23:09 -0300

    Fix(animationPlay): Make animation play automatically asa its loaded
    Signed-off-by: Murilo Araujo <muernandes@icloud.com>

--- AnimationComponent.swift
+++ AnimationComponent.swift
@@ -12,13 +12,54 @@ import Lottie
 
 public struct AnimationComponent: ServerDrivenComponent {
     let url: String
+    
+    public init(url: String) {
+        self.url = url
+    }
+    
     public func toView(renderer: BeagleRenderer) -> UIView {
         guard let url = URL(string: url) else {
             return UIView()
         }
-        let animationView = AnimationView(url: url) { (error) in
-            
-        }
+        
+        let animationView = CustomLotView(url: url)
+        animationView.frame = CGRect(x: animationView.frame.minX, y: animationView.frame.minY, width: animationView.frame.width, height: 200)
         return animationView
+    }
+    
+    public static func register() {
+        Beagle.registerCustomComponent("AnimationView", componentType: AnimationComponent.self)
+    }
+}
+
+class CustomLotView: UIView {
+    var animationView: AnimationView
+    
+    init(url: URL) {
+        animationView = AnimationView()
+        super.init(frame: .zero)
+        animationView = AnimationView(url: url) { [weak self] error in
+            self?.playAnimation()
+        }
+        animationView.loopMode = .loop
+        animationView.contentMode = .scaleAspectFit
+        addSubview(animationView)
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        [
+            animationView.topAnchor.constraint(equalTo: topAnchor),
+            animationView.leftAnchor.constraint(equalTo: leftAnchor),
+            animationView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            animationView.rightAnchor.constraint(equalTo: rightAnchor)
+        ].forEach { (constraint) in
+            constraint.isActive = true
+        }
+    }
+    
+    func playAnimation() {
+        animationView.play()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
--- Example/AnimationComponent/ViewController.swift
+++ Example/AnimationComponent/ViewController.swift
@@ -7,17 +7,39 @@
 //
 
 import UIKit
+import Beagle
+import AnimationComponent
 
 class ViewController: UIViewController {
 
+    let button: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Open component", for: .normal)
+        btn.setTitleColor(UIColor.blue, for: .normal)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(button)
+        [
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ].forEach { (item) in
+            item.isActive = true
+        }
+        
+        button.addTarget(self, action: #selector(openAnimationComponentVC), for: .touchUpInside)
     }
 
+    @objc func openAnimationComponentVC() {
+        let beagleScreenVc = Beagle.screen(.declarative(.init(child: AnimationComponent(url: "https://assets7.lottiefiles.com/packages/lf20_c9miik7h.json"))))
+        self.present(beagleScreenVc, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
